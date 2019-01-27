ada8583f55164d1d709cdb61bc9fc32624605212
Adrian Widerski <awiderski@outlook.com>
2019-01-27 01:59:17 +0100

    Some functions work, CSS configs

--- functions.php
+++ functions.php
@@ -9,3 +9,11 @@ add_filter('use_block_editor_for_post', '__return_false');
 
 // Enable post thumbnails in theme
 add_theme_support( 'post-thumbnails' ); 
+
+// Register styles and scripts
+$version = '0.1';
+$mainJS = get_template_directory_uri() . '/dist/js/main.js';
+$mainCSS = get_template_directory_uri() . '/style.css';
+
+wp_enqueue_style( 'style', $mainCSS, $version, true );
+wp_enqueue_script( 'script', $mainJS, $version, true);
--- index.php
+++ index.php
@@ -1,3 +1,6 @@
-<?php 
+<?php get_header('./parts/base/header'); ?>
 
-    
+	<!-- HTML here -->
+
+<?php // get_sidebar(); ?>
+<?php get_footer('./parts/base/footer'); ?>
