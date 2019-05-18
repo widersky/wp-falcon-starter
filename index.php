0696d2580d20d79f8b997058df399690f6b148b6
Adrian Widerski <awiderski@outlook.com>
2019-05-18 20:40:51 +0200

    Some grid enhancements

--- index.php
+++ index.php
@@ -8,11 +8,13 @@
         <div class="row">
             <div class="col-100">
                 <h1>You just successfully installed WP Starter Theme! 🎉</h1>
-                <h5>This demo shows included PerseusCSS framework. Feel free to use something else, if you want :)</h5>
+                <h5>This demo shows included PerseusCSS framework. Feel free to use something else, if you want but... just look at this, isn't it sufficient?</h5>
             </div>
         </div>
     </header>
 
+
+    <!-- 1. GRID -->
     <section class="section">
         <div class="row">
             <div class="col-100">
@@ -21,7 +23,7 @@
         </div>
     </section>
 
-    <section class="section section--docs">
+    <section class="section">
         <div class="row">
             <div class="col-100">
                 <div class="docs">
@@ -83,6 +85,59 @@
         </div>
         <div class="row">
             <div class="col-100"><span>100%</span></div>
+        </div>
+        <div class="row">
+            <div class="col"><span>auto</span></div>
+            <div class="col"><span>auto</span></div>
+            <div class="col"><span>auto</span></div>
+        </div>
+    </section>
+
+
+    <!-- 2. GRID MANIPULATION -->
+    <section class="section">
+        <div class="row">
+            <div class="col-100">
+                <h2>2. Grid manipulation</h2>
+            </div>
+        </div>
+    </section>
+
+    <section class="section section--cols">
+
+        <!-- Pull right -->
+        <div class="row">
+            <div class="col-100">
+                <div class="docs">
+                    <p class="info info--neutral">
+                        Use <code>row--pull-right</code> modifier to stick columns to right side of row.
+                    </p>
+                </div>
+            </div>
+        </div>
+        <div class="row row--pull-right">
+            <div class="col-25"><span>25% pulled right</span></div>
+        </div>
+        <div class="row row--pull-right">
+            <div class="col-50"><span>50% pulled right</span></div>
+        </div>
+
+        <!-- Center -->
+        <div class="row">
+            <div class="col-100">
+                <div class="docs">
+                    <p class="info info--neutral">
+                        Use <code>row--center</code> modifier to center columns inside row.
+                    </p>
+                </div>
+            </div>
+        </div>
+        <div class="row row--center">
+            <div class="col-75"><span>75% centered</span></div>
+        </div>
+        <div class="row row--center">
+            <div class="col-25"><span>First 25% centered</span></div>
+            <div class="col-25"><span>Second 25% centered</span></div>
         </div>
     </section>
 
